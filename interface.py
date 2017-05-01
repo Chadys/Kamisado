1fd2ac8562af01d9e000891fed7d0f6393ce7d14
Chadys <chadys@hotmail.fr>
2017-05-01 20:35:39 +0200

    refractoring of error print

--- interface.py
+++ interface.py
@@ -5,12 +5,12 @@ from subprocess import Popen, PIPE, STDOUT
 from nbstreamreader import NonBlockingStreamReader as NBSR
 import time
 
-PROG_IA1 = 0
-PROG_IA2 = 1
+PROG_AI1 = 0
+PROG_AI2 = 1
 PROG_REF = 2
 PROG_DIS = 3
 
-TIMEOUT_IA = 10
+TIMEOUT_AI = 10
 TIMEOUT_HUMAN = None
 TIMEOUT_DISPLAYER = 3
 TIMEOUT_REF = 5
@@ -32,13 +32,13 @@ def end(ia_):
         print(RED+"{} won !".format(PROCESSES[ia_]['name'])+RESET)
         if len(PROCESSES) == 4:
             if communicate(PROCESSES[PROG_DIS], 'endgame ' + str(ia_) + '\n', 2.0, except_none=True) != "OK":
-                print('DISPLAYER failed for [endgame] command.')
-            time.sleep(5)
+                print_fail(PROG_DIS, "endgame")
+            time.sleep(25)
     print("Exiting... ({})".format(ia_))
     for i, process in enumerate(PROCESSES):
         if process['type'] == COMP:
             if communicate(process, 'quit\n', 0.5, except_none=True) != "OK":
-                print('PROG_{} failed for [quit] command.\n'.format(i))
+                print_fail(i, "quit")
             process['process'].kill()
     exit()
 
@@ -67,54 +67,60 @@ def init_processes(length):
             #NBSR(process.stdin, doprint=True, idd=i+9)
             PROCESSES.append({'type':COMP, 'process':process, 'nbsr':nbsr, 'id':i - 1})
 
+    PROCESSES[PROG_AI1]['name'] = "AI1"
+    PROCESSES[PROG_AI2]['name'] = "AI2"
+    PROCESSES[PROG_REF]['name'] = "REFEREE"
+    if length == 4:
+        PROCESSES[PROG_DIS]['name'] = "DISPLAYER"
     if not communicate(PROCESSES[PROG_REF], 'init\n', 2, except_none=True) == "OK":
-        print('REFEREE failed for [init] command.')
+        print_fail(PROG_REF, "init")
 
 
-    if PROCESSES[PROG_IA1]['type'] == HUMAN:
+    if PROCESSES[PROG_AI1]['type'] == HUMAN:
         val1 = 1
     else:
-        val1 = communicate(PROCESSES[PROG_IA1], 'init\n', 2, except_none=True) == "OK"
+        val1 = communicate(PROCESSES[PROG_AI1], 'init\n', 2, except_none=True) == "OK"
 
-    if PROCESSES[PROG_IA2]['type'] == HUMAN:
+    if PROCESSES[PROG_AI2]['type'] == HUMAN:
         val2 = 1
     else:
-        val2 = communicate(PROCESSES[PROG_IA2], 'init\n', 2, except_none=True) == "OK"
+        val2 = communicate(PROCESSES[PROG_AI2], 'init\n', 2, except_none=True) == "OK"
 
     for i, val in enumerate([val1, val2]):
         if not val:
-            print('PROG_{} failed for [init] command.'.format(i))
+            print_fail(i, "init")
     check = complicated(val1, val2)
     if check < 2:
         end(check)
 
-
-    if PROCESSES[PROG_IA1]['type'] == HUMAN:
-        name1 = "Human 1"
+    first_human = False;
+    if PROCESSES[PROG_AI1]['type'] == HUMAN:
+        name1 = "Human"
+        first_human = True
     else:
-        name1 = communicate(PROCESSES[PROG_IA1], 'name\n', 2)
+        name1 = communicate(PROCESSES[PROG_AI1], 'name\n', 2)
 
-    if PROCESSES[PROG_IA2]['type'] == HUMAN:
-        name2 = "Human 2"
+    if PROCESSES[PROG_AI2]['type'] == HUMAN:
+        name2 = "Human 2" if first_human else "Human"
     else:
-        name2 = communicate(PROCESSES[PROG_IA2], 'name\n', 2)
+        name2 = communicate(PROCESSES[PROG_AI2], 'name\n', 2)
 
     val1 = name1 is not None
     val2 = name2 is not None
     for i, val in enumerate([val1, val2]):
         if not val:
-            print('PROG_{} failed for [name] command.'.format(i))
+            print_fail(i, "name")
     check = complicated(val1, val2)
     if check < 2:
         end(check)
-    PROCESSES[PROG_IA1]['name'] = name1
-    PROCESSES[PROG_IA2]['name'] = name2
+    PROCESSES[PROG_AI1]['name'] = name1
+    PROCESSES[PROG_AI2]['name'] = name2
 
     if length == 5:
         if not communicate(PROCESSES[PROG_DIS], 'init\n', 2, except_none=True) == "OK":
-            print('DISPLAYER failed for [init] command.')
+            print_fail(PROG_DIS, "init")
         if communicate(PROCESSES[PROG_DIS], 'names {};{}\n'.format(name1, name2), 2.0, except_none=True) != "OK":
-            print('DISPLAYER failed for [names] command.')
+            print_fail(PROG_DIS, "names")
 
 def communicate(program_, command_, timeout_, except_none=False):
     """Send command to a program and return his answer"""
@@ -135,7 +141,7 @@ def communicate(program_, command_, timeout_, except_none=False):
 
 
 def get_ref(command_, timeout_):
-    """Get the referee's answer for an IA move"""
+    """Get the referee's answer for an AI's move"""
     res = communicate(PROCESSES[PROG_REF], command_, timeout_)
     if res:
         res = int(''.join(res))
@@ -148,7 +154,7 @@ def get_ref(command_, timeout_):
 def ask_ia_or_human(_ia):
     """Ask for genmove to process"""
     if PROCESSES[_ia]['type'] == COMP:
-        res = communicate(PROCESSES[_ia], 'genmove\n', TIMEOUT_IA)
+        res = communicate(PROCESSES[_ia], 'genmove\n', TIMEOUT_AI)
     else:
         print("ASKING TO HUMAN")
         res = communicate(PROCESSES[PROG_DIS], 'genmove\n', TIMEOUT_HUMAN)
@@ -174,7 +180,7 @@ def main_loop(use_display, c_ia):
         print(BLUE+'Sending [genmove] to {}'.format(PROCESSES[c_ia]['name'])+RESET)
         res_ia = ask_ia_or_human(c_ia)
         if not res_ia:
-            print("{} failed for [genmove] command\n".format(PROCESSES[c_ia]['name']))
+            print_fail(c_ia, "genmove")
             end(c_adv)
 
         print('{} anwser [{}]'.format(PROCESSES[c_ia]['name'], res_ia))
@@ -182,21 +188,22 @@ def main_loop(use_display, c_ia):
         res_ref = get_ref('move {}\n'.format(res_ia), 8.0)
         print('REFEREE answer [{}]'.format(res_ref))
         if res_ref is None or res_ref < 0 or res_ref > 2:
-            print('REFEREE failed for [move] command.')
+            print_fail(PROG_REF, "move")
             end(-1)
         if res_ref == 0:
-            print("{} failed for [genmove] command (illegal move)\n".format(PROCESSES[c_ia]['name']))
+            print_fail(c_ia, "genmove")
+            print("REFEREE decided it was illegal.\n")
             end(c_adv)
         elif res_ref == 2:
             if use_display:
                 print('Sending [move {}] to DISPLAYER'.format(res_ia))
                 if communicate(PROCESSES[PROG_DIS], 'move ' + res_ia + '\n', 0.5, except_none=True) != "OK":
-                    print('DISPLAYER failed for [move] command\n')
+                    print_fail(PROG_DIS, "move")
             end(c_ia)
 
         print(BLUE+'Sending [move {}] to {}'.format(res_ia, PROCESSES[c_adv]['name'])+RESET)
         if not notify_ia(c_adv, 'move {}\n'.format(res_ia)):
-            print("{} failed for [move] command\n".format(PROCESSES[c_adv]['name']))
+            print_fail(c_adv, "move")
             end(c_ia)
         else:
             print("{} OK".format(PROCESSES[c_adv]['name']))
@@ -204,11 +211,13 @@ def main_loop(use_display, c_ia):
         if use_display:
             print('Sending [move {}] to DISPLAYER'.format(res_ia))
             if communicate(PROCESSES[PROG_DIS], 'move ' + res_ia + '\n', 0.5, except_none=True) != "OK":
-                print('DISPLAYER failed for [move] command\n')
+                print_fail(PROG_DIS, "move")
 
         c_ia = c_adv
         c_adv = int(not c_ia)
 
+def print_fail(index_prog, name_command):
+    print('{} failed for [{}] command.\n'.format(PROCESSES[index_prog]['name'], name_command))
 
 
 if __name__ == "__main__":
